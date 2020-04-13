0bb4d66f82b45685eb3f9ac3118a0cae14776fda
Benjamin Eberlei <kontakt@beberlei.de>
2020-04-13 19:21:24 +0200

    Move code from zend_default_classes.c to zend_attributes.c

--- Zend/zend_attributes.h
+++ Zend/zend_attributes.h
@@ -7,10 +7,6 @@ zend_class_entry *zend_ce_php_compiler_attribute;
 typedef void (*zend_attributes_internal_validator)(zval *attribute);
 HashTable zend_attributes_internal_validators;
 
-static void zend_compiler_attribute_register(zend_class_entry *ce, zend_attributes_internal_validator *validator)
-{
-	zend_string *attribute_name = zend_string_tolower_ex(ce->name, 1);
-
-	zend_hash_update_mem(&zend_attributes_internal_validators, attribute_name, validator, sizeof(zend_attributes_internal_validator));
-}
+void zend_compiler_attribute_register(zend_class_entry *ce, zend_attributes_internal_validator *validator);
+void zend_register_attribute_ce(void);
 #endif
--- Zend/zend_default_classes.c
+++ Zend/zend_default_classes.c
@@ -27,37 +27,6 @@
 #include "zend_generators.h"
 #include "zend_weakrefs.h"
 
-void zend_attribute_validate_phpattribute(zval *attribute)
-{
-}
-
-void zend_attribute_validate_phpcompilerattribute(zval *attribute)
-{
-	zend_error(E_COMPILE_ERROR, "The PhpCompilerAttribute can only be used by internal classes, use PhpAttribute instead");
-}
-
-static void zend_register_attribute_ce(void)
-{
-	zend_hash_init(&zend_attributes_internal_validators, 8, NULL, NULL, 1);
-
-	zend_class_entry ce;
-	zend_attributes_internal_validator cb;
-
-	INIT_CLASS_ENTRY(ce, "PhpAttribute", NULL);
-	zend_ce_php_attribute = zend_register_internal_class(&ce);
-	zend_ce_php_attribute->ce_flags |= ZEND_ACC_FINAL;
-
-	cb = zend_attribute_validate_phpattribute;
-	zend_compiler_attribute_register(zend_ce_php_attribute, &cb);
-
-	INIT_CLASS_ENTRY(ce, "PhpCompilerAttribute", NULL);
-	zend_ce_php_compiler_attribute = zend_register_internal_class(&ce);
-	zend_ce_php_compiler_attribute->ce_flags |= ZEND_ACC_FINAL;
-
-	cb = zend_attribute_validate_phpcompilerattribute;
-	zend_compiler_attribute_register(zend_ce_php_compiler_attribute, &cb);
-}
-
 ZEND_API void zend_register_default_classes(void)
 {
 	zend_register_interfaces();
